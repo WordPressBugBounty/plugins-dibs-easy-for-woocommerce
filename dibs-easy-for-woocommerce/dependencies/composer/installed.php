3d6be8cfc7f6c2a5f3fbbdbedf5435d9dbc403ff
Sean Murphy <sean@iamseanmurphy.com>
2025-02-17 11:17:18 +0000

    2.10.1

---
+++ dibs-easy-for-woocommerce/dependencies/composer/installed.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace KrokedilNexiCheckoutDeps;
+
+return array('root' => array('name' => '__root__', 'pretty_version' => '2.10.1', 'version' => '2.10.1.0', 'reference' => 'ee3f80be075b95c31a7b34b168f1f549be9417cd', 'type' => 'library', 'install_path' => __DIR__ . '/../../', 'aliases' => array(), 'dev' => \true), 'versions' => array('__root__' => array('pretty_version' => '2.10.1', 'version' => '2.10.1.0', 'reference' => 'ee3f80be075b95c31a7b34b168f1f549be9417cd', 'type' => 'library', 'install_path' => __DIR__ . '/../../', 'aliases' => array(), 'dev_requirement' => \false), 'krokedil/woocommerce' => array('pretty_version' => '1.7.1', 'version' => '1.7.1.0', 'reference' => 'e2d32e503cfdbcfd574dbb6f06c5e5aaa3c87a2c', 'type' => 'library', 'install_path' => __DIR__ . '/../krokedil/woocommerce', 'aliases' => array(), 'dev_requirement' => \false)));
