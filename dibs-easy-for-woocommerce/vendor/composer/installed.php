4bf64155afee67b055a5e4d6b74e2d6ee5a9593b
Sean Murphy <sean@iamseanmurphy.com>
2026-04-08 07:46:41 +0000

    2.14.1

--- dibs-easy-for-woocommerce/vendor/composer/installed.php
+++ dibs-easy-for-woocommerce/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'krokedil/dibs-easy-for-woocommerce',
-        'pretty_version' => '2.14.0',
-        'version' => '2.14.0.0',
-        'reference' => '89d8692cab109ca557b5a0b3a153243eaa698f6c',
+        'pretty_version' => '2.14.1',
+        'version' => '2.14.1.0',
+        'reference' => '2b0a2a42f6f0c0492b59b44297fbb42d2ccb5e92',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'krokedil/dibs-easy-for-woocommerce' => array(
-            'pretty_version' => '2.14.0',
-            'version' => '2.14.0.0',
-            'reference' => '89d8692cab109ca557b5a0b3a153243eaa698f6c',
+            'pretty_version' => '2.14.1',
+            'version' => '2.14.1.0',
+            'reference' => '2b0a2a42f6f0c0492b59b44297fbb42d2ccb5e92',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
